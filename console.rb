50d105012a01ee4dbdf7b0e30057419cd9ec035a
kit.perez <kit.perez@gmail.com>
2017-07-05 12:44:12 +1000

    Initial commit: - added sample of a graph chosen for displaying stock charts - added user and stock models - added index, login, and result erbs

---
+++ console.rb
@@ -0,0 +1,8 @@
+require 'pry'
+require 'active_record'
+
+require_relative 'models/user'
+require_relative 'models/stock'
+require_relative 'db_config'
+
+binding.pry
---
+++ main.rb
@@ -0,0 +1,41 @@
+require 'pry'
+require 'sinatra'
+require 'sinatra/reloader'
+require 'rubygems'
+require 'market_beat'
+require 'simple_statistics'
+require 'HTTParty'
+
+get '/' do
+  erb :index
+end
+
+# http://localhost:4567/stock?stock=APPL
+get '/stock' do
+  # result = YahooFinance::get_quotes(YahooFinance::StandardQuote, params[:stock])
+  # HTTParty.get('https://www.quandl.com/api/v3/datasets/WIKI/AAPL.json')
+  request_link ='https://www.quandl.com/api/v3/datasets/WIKI/ #{params[:stock]}.json'
+  result = HTTParty.get('https://www.quandl.com/api/v3/datasets/WIKI/AAPL.json')
+  @data = result[ "data" ]
+
+  # beat_result = MarketBeat.quotes(:AAPL, "20011-12-21", "2011-12-22")
+  binding.pry
+
+# zESs-hBzxtczjAM_Jcds
+  # output in html
+  erb :result
+end
+
+get '/login' do
+  erb :login
+end
+
+get '/about' do
+  'about me'
+end
+
+
+# /api/stock-prices?stock=goog
+# /api/stock-prices?stock=goog&like=true
+# /api/stock-prices?stock=goog&stock=msft
+# /api/stock-prices?stock=goog&stock=msft&like=true
